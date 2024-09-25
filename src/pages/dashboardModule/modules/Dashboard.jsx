3d7cc900c1e1ba2e68ecaa5217f3b6bc04c72c8d
saran.baral <saranbrl35@gmail.com>
2024-09-25 16:59:08 +0545

    cards responsiveness integration

--- src/pages/dashboardModule/modules/Dashboard.jsx
+++ src/pages/dashboardModule/modules/Dashboard.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import { memo } from 'react'
 import ContentWrapper from '../../../shared/ContentWrapper'
 
 const Dashboard = () => {
   return (
     <ContentWrapper title="Dashboardd">
-      <div>
-        <div className="card">
-          asdasdasdasd
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-9 gap-[18px]">
+        <SalesCard />
+        <ProfitCard />
+        <CountryStatCard />
       </div>
     </ContentWrapper>
   )
 }
 
 export default Dashboard
+
+const SalesCard = memo(function TotalSalesCard() {
+  return <div className="card col-span-1 md:col-span-3">
+    Total Sales
+  </div>
+})
+const ProfitCard = memo(function TotalSalesCard() {
+  return <div className="card col-span-1 md:col-span-2">
+    Total Profit
+  </div>
+})
+const CountryStatCard = memo(function TotalSalesCard() {
+  return <div className="card col-span-1 md:col-span-4">
+    Total Profit
+  </div>
+})
