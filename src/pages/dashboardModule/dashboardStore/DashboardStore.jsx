075d5a6a68a508ce7a76180c2dbe89e68d75e9bb
saran.baral <saranbrl35@gmail.com>
2024-09-26 16:09:15 +0545

    dashboard global state init

---
+++ src/pages/dashboardModule/dashboardStore/DashboardStore.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+const initialState = {};
+export const ContextDashboard = React.createContext(initialState);
+export const ProviderDashboard = ({ children }) => {
+  const [ordersActiveMenu, setOrdersActiveMenu] = useState("all");
+  const _orderMenus = [
+    {
+      label: "All Orders",
+      active: ordersActiveMenu === "all",
+      onClick: () => setOrdersActiveMenu("all"),
+    },
+    {
+      label: "Completed",
+      active: ordersActiveMenu === "completed",
+      onClick: () => setOrdersActiveMenu("completed"),
+    },
+    {
+      label: "Canceled",
+      active: ordersActiveMenu === "canceled",
+      onClick: () => setOrdersActiveMenu("canceled"),
+    },
+  ];
+
+  return (
+    <ContextDashboard.Provider
+      value={{
+        _orderMenus,
+      }}
+    >
+      {children}
+    </ContextDashboard.Provider>
+  );
+};
+ProviderDashboard.propTypes = {
+  children: PropTypes.node,
+};
