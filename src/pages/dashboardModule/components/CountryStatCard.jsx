85e0c07509ad389bceb2435f78e3647683865aa5
saran.baral <saranbrl35@gmail.com>
2024-09-26 11:25:01 +0545

    integrated country stats cards with mock data

--- src/pages/dashboardModule/components/CountryStatCard.jsx
+++ src/pages/dashboardModule/components/CountryStatCard.jsx
@@ -1,6 +1,42 @@
 import { memo } from "react";
-
+import { AUSIcon, BrazilIcon, USAIcon } from "../../../utils";
+import CountryWiseCardStat from "./CountryWiseCardStat";
 const CountryStatCard = memo(function TotalSalesCard() {
-  return <div className="card col-span-1 lg:col-span-4 dashboard-card-wrapper">Total Profit</div>;
+  const _countryInfosUSA = {
+    icon: USAIcon,
+    title: "United States",
+    marginAmount: "30k",
+    percentage: "25.8%",
+    isProfit: true,
+  };
+  const _countryInfosBra = {
+    icon: BrazilIcon,
+    title: "Brazil",
+    marginAmount: "26k",
+    percentage: "16.2%",
+    isProfit: false,
+  };
+  const _countryInfosAUS = {
+    icon: AUSIcon,
+    title: "Australia",
+    marginAmount: "17k",
+    percentage: "11.9%",
+    isProfit: false,
+  };
+  return (
+    <div className="card col-span-1 lg:col-span-4 dashboard-card-wrapper">
+      <ul className="space-y-[18px]">
+        <li>
+          <CountryWiseCardStat {..._countryInfosUSA} />
+        </li>
+        <li>
+          <CountryWiseCardStat {..._countryInfosBra} />
+        </li>
+        <li>
+          <CountryWiseCardStat {..._countryInfosAUS} />
+        </li>
+      </ul>
+    </div>
+  );
 });
 export default CountryStatCard;
