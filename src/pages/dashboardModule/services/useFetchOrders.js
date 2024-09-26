38a859618516a6f2be1c3715d96daf0dcdfcf4e0
saran.baral <saranbrl35@gmail.com>
2024-09-26 17:11:27 +0545

    fetching orders listing hooks init

--- src/pages/dashboardModule/services/useFetchOrders.js
+++ src/pages/dashboardModule/services/useFetchOrders.js
@@ -1,8 +1,14 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { _mockOrdersData } from "../constants";
+import { useEffectUpdate } from "../../../hooks";
 
 const useFetchOrders = () => {
   const [orders, setOrders] = useState([]);
+  const [filters, setFilters] = useState({
+    limit: 10,
+    page: 1,
+    total: 0,
+  });
   const [processing, setProcessing] = useState(true);
   const fetchAllOrders = () => {
     setProcessing(true);
@@ -11,15 +17,21 @@ const useFetchOrders = () => {
       .then((res) => {
         if (res?.data) {
           setOrders(res?.data);
+          setFilters((pre) => ({ ...pre, total: res?.data?.length }));
         }
       })
-      .catch(() => setOrders(_mockOrdersData?.data))
+      .catch(() => {
+        setOrders(_mockOrdersData?.data);
+        setFilters((pre) => ({ ...pre, total: _mockOrdersData?.data?.length }));
+      })
       .finally(() => setProcessing(false));
   };
-  useEffect(() => {
-    fetchAllOrders();
-  }, []);
-  return { orders, processing };
+  useEffectUpdate(
+    useCallback(() => {
+      fetchAllOrders();
+    }, [filters.limit, filters.page])
+  );
+  return { orders, processing, setFilters, filters };
 };
 
 export default useFetchOrders;
