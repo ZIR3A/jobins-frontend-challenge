0eb0e268570ac9634f8bbddbfe3c683f31fca0be
saran.baral <saranbrl35@gmail.com>
2024-09-26 13:38:38 +0545

    menus addition

--- src/pages/dashboardModule/modules/Dashboard.jsx
+++ src/pages/dashboardModule/modules/Dashboard.jsx
@@ -3,9 +3,27 @@ import SalesCard from "../components/SalesCard";
 import ProfitCard from "../components/ProfitCard";
 import CountryStatCard from "../components/CountryStatCard";
 import PersonalSalesInfo from "../components/PersonalSalesInfo";
-
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [ordersActiveMenu, setOrdersActiveMenu] = useState("all");
+  const _orderMenus = [
+    {
+      label: "All Orders",
+      active: ordersActiveMenu === "all",
+      onClick: () => setOrdersActiveMenu("all"),
+    },
+    {
+      label: "Completed",
+      active: ordersActiveMenu === "completed",
+      onClick: () => setOrdersActiveMenu("completed"),
+    },
+    {
+      label: "Canceled",
+      active: ordersActiveMenu === "canceled",
+      onClick: () => setOrdersActiveMenu("canceled"),
+    },
+  ];
   return (
     <ContentWrapper title="Dashboardd">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-9 gap-[18px]">
@@ -16,6 +34,13 @@ const Dashboard = () => {
       <div className="mt-4">
         <div className="card px-10 py-5">
           <PersonalSalesInfo />
+          <nav className="mt-6 md:mt-2">
+            {_orderMenus?.map(({ active, label, onClick }, _index) => (
+              <span key={_index} className={`text-nowrap cursor-pointer py-6 px-1 md:px-6 text-[15px] ${active ? "border-b-2 border-blue-primary text-blue-primary" : "text-secondary"}`} onClick={onClick}>
+                {label}
+              </span>
+            ))}
+          </nav>
         </div>
       </div>
     </ContentWrapper>
@@ -23,4 +48,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
