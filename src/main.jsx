149876bb29c8056e231e30e15d8c21006602c016
saran.baral <saranbrl35@gmail.com>
2024-09-26 17:26:38 +0545

    strict mode init

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import "flowbite";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'flowbite'
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
