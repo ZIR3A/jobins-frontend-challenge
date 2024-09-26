2af82a41d6ebfc535386aee35dce04bcc6eaa0c7
saran.baral <saranbrl35@gmail.com>
2024-09-26 14:25:44 +0545

    styling updated

--- src/shared/Dropdown.jsx
+++ src/shared/Dropdown.jsx
@@ -4,17 +4,17 @@ import { memo } from "react";
 const Dropdown = memo(function Dropdown({ className = "", label = "", options = [] }) {
   return (
     <>
-      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className={`${className} flex items-center`} type="button">
+      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className={`${className} flex items-center border-0 focus:ring-blue-primary focus:border-blue-primary `} type="button">
         {label}
         <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
         </svg>
       </button>
-      <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <div id="dropdown" className="z-10 hidden bg-white divide-y divide-tertiary rounded-lg shadow w-44">
+        <ul className="py-2 text-sm text-primary" aria-labelledby="dropdownDefaultButton">
           {options?.map((_option, _i) => (
             <li key={_i}>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+              <a href="#" className="block px-4 py-2 hover:bg-tertiary">
                 {_option?.label}
               </a>
             </li>
